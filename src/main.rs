3aafb8bf36c73113a249cf58c960b59e4b1b7e8a
Justin <ninjagecko5000@gmail.com>
2025-05-26 04:02:44 -0700

    map segment bits for serially controlled section

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use z31_hvac::temp::Thermistor;
 use z31_hvac::*;
 
 use core::cell::RefCell;
+use core::u128;
 use embassy_embedded_hal::shared_bus::blocking::spi::SpiDeviceWithConfig;
 use embassy_executor::Spawner;
 use embassy_rp::bind_interrupts;
@@ -17,7 +18,7 @@ use embassy_rp::spi;
 use embassy_rp::spi::Spi;
 use embassy_sync::blocking_mutex::Mutex;
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
-use embassy_time::{Duration, Ticker};
+use embassy_time::{block_for, Duration, Ticker};
 use embassy_rp::adc::{Adc, Channel, Config};
 
 use display::Display;
@@ -54,28 +55,30 @@ async fn main(_spawner: Spawner) {
     let adc = Adc::new_blocking(p.ADC, Config::default());
 
     let ambtemp = Channel::new_pin(p.PIN_26, Pull::None);
+    let onboardts = Channel::new_temp_sensor(p.ADC_TEMP_SENSOR);
 
-    let mut therm = Thermistor::new(ambtemp, adc);
+    let mut therm = Thermistor::new(ambtemp, onboardts, adc);
 
     let sclk = p.PIN_22;
     let mosi = p.PIN_23;
     let miso = p.PIN_20;
+    
     let cs = Output::new(p.PIN_25, Level::High);
     let rst = Output::new(p.PIN_24, Level::High);
 
-    let mut config = spi::Config::default();
-    config.frequency = 4_000_000;
+    //let mut config = spi::Config::default();
+    //config.frequency = 4_000_000;
 
-    let mut config1 = spi::Config::default();
-    config1.frequency = 4_000_000;
+    //let mut config1 = spi::Config::default();
+    //config1.frequency = 4_000_000;
 
-    let spi = Spi::new_blocking(p.SPI0, sclk, mosi, miso, config);
+    //let spi = Spi::new_blocking(p.SPI0, sclk, mosi, miso, config);
 
-    let spibus: Mutex<CriticalSectionRawMutex, RefCell<_>> = Mutex::new(RefCell::new(spi));
+    //let spibus: Mutex<CriticalSectionRawMutex, RefCell<_>> = Mutex::new(RefCell::new(spi));
 
-    let vfd_spi = SpiDeviceWithConfig::new(&spibus, cs, config1);
+    //let vfd_spi = SpiDeviceWithConfig::new(&spibus, cs, config1);
 
-    let mut vfd = Display::new(vfd_spi, rst);
+    //let mut vfd = Display::new(vfd_spi, rst);
 
     //vfd.set_brightness(128).unwrap();
     // let mut ticker = Ticker::every(Duration::from_secs(1));
@@ -86,19 +89,40 @@ async fn main(_spawner: Spawner) {
     let mut ws2812 = PioWs2812::new(&mut common, sm0, p.DMA_CH1, p.PIN_21, &program);*/
 
     // Wrap flash as block device
-    let mut ticker = Ticker::every(Duration::from_millis(25));
+    let mut ticker = Ticker::every(Duration::from_millis(1000));
+    let mut offdelay = Ticker::every(Duration::from_micros(8));
+    let mut ontime = Ticker::every(Duration::from_micros(2));
 
-    vfd.draw_boot_image().await;
+    //vfd.draw_boot_image().await;
+    let mut clock = Output::new(p.PIN_6, Level::Low);
+    let mut data = Output::new(p.PIN_5, Level::Low);
 
     loop {
+        let bitfield: u128 = 0x1FFFFFFFFFF;
 
+        for i in 0..128 {
+        if (bitfield >> i) & 1 == 1 {
+            let single_bit: u128 = 1u128 << i;
 
+            for i in (0..128).rev(){
+            clock.set_low();
+            block_for(Duration::from_micros(8));
+            clock.set_high();
+            block_for(Duration::from_micros(2));
+            let gpio_level = (single_bit >> i) & 1 !=0;
+            data.set_level(gpio_level.into());
+        }
+        block_for(Duration::from_millis(1000));
+    }
+    }
 
-        vfd.ambient_temp = therm.measure_temp() as i8;
+        //let vals = therm.measure_temp();
+        //vfd.ambient_temp = vals[0] as i8;
+        //vfd.internal_temp = vals[1] as i8;
 
-        vfd.update_display();
+        //vfd.update_display();
 
-        ticker.next().await;
+        //ticker.next().await;
         /*for j in 0..(256 * 5) {
             for i in 0..NUM_LEDS {
                 data[i] = wheel((((i * 256) as u16 / NUM_LEDS as u16 + j as u16) & 255) as u8);
--- src/temp.rs
+++ src/temp.rs
@@ -27,6 +27,14 @@ pub fn celcius_to_fahrenheit(celsius: f64) -> f64 {
     celsius * 1.8 + 32.0
 }
 
+fn onboard_temp_calculate(adcin: f64) -> f64{
+        // According to chapter 12.4.6 Temperature Sensor in RP235x datasheet
+        let temp = 27.0 - (adcin * 3.3 / 4096.0 - 0.706) / 0.001721;
+        let sign = if temp < 0.0 { -1.0 } else { 1.0 };
+        let rounded_temp_x10 = (temp * 10.0) + 0.5 * sign;
+        (rounded_temp_x10) / 10.0
+}
+
 pub struct Thermistor<'a>{
     /// pull-up resistor in the divider (Ω)
     pullup: f64,
@@ -39,15 +47,17 @@ pub struct Thermistor<'a>{
 
     adc_max: f64,
 
-    sensor: Channel<'a>,
+    sensor1: Channel<'a>,
+
+    sensor2: Channel<'a>,
 
     adc: Adc<'a, Blocking>,
 }
 
 impl <'a>Thermistor<'a> {
-    pub fn new(sensor: Channel<'a>, adc: Adc<'a, Blocking>) -> Self {
-        let pullup = 47_000.0;
-        let r0 = 100_000.0;
+    pub fn new(sensor1: Channel<'a>, sensor2: Channel<'a>, adc: Adc<'a, Blocking>) -> Self {
+        let pullup = 10_000.0;
+        let r0 = 10_000.0;
         let t0_k = celsius_to_kelvin(25.0); // celcius to kelvin
         let beta = 3950.0;
         let adc_max = 4095.0;
@@ -58,29 +68,38 @@ impl <'a>Thermistor<'a> {
             t0_k,
             beta,
             adc_max,
-            sensor,
+            sensor1,
+            sensor2,
             adc,
         }
     }
 
-    pub fn measure_adc(&mut self) -> f64{
-        let mut samples: [u16; 128] = [0u16; 128];
-        for sample in &mut samples{
-            let _ = self.adc.blocking_read(& mut self.sensor);
-            *sample = self.adc.blocking_read(& mut self.sensor).unwrap();
+    pub fn measure_adcs(&mut self) -> [f64; 2]{
+        let mut samples1: [u16; 128] = [0u16; 128];
+        for sample in &mut samples1{
+            *sample = self.adc.blocking_read(& mut self.sensor1).unwrap();
         }
-        let average: f64 = average(&samples).into();
-        average
+        let average1: f64 = average(&samples1).into();
+        let mut samples2: [u16; 128] = [0u16; 128];
+        for sample in &mut samples2{
+            *sample = self.adc.blocking_read(& mut self.sensor2).unwrap();
+        }
+        let average2: f64 = average(&samples2).into();
+        [average1,average2]
     }
 
-    pub fn measure_temp(&mut self) -> f64{
-        let adc = self.measure_adc();
-        let res = self.adc_to_resistance(adc);
-        let temp = self.calculate_temperature(res);
-        let tempc = kelvin_to_celsius(temp);
-        let tempf = celcius_to_fahrenheit(tempc);
-        tempf
+    pub fn measure_temp(&mut self) -> [f64; 2]{
+        let adc = self.measure_adcs();
+        let res1 = self.adc_to_resistance(adc[0]);
+        let temp1 = self.calculate_temperature(res1);
+        let tempc1 = kelvin_to_celsius(temp1);
+        let tempf1 = celcius_to_fahrenheit(tempc1);
+        let res2 = onboard_temp_calculate(adc[1]);
+        let tempf2 = celcius_to_fahrenheit(res2);
+        [tempf1, tempf2]
     }
+
+
 
     /// Convert a raw 16-bit ADC reading → thermistor resistance (Ω)
     fn adc_to_resistance(&self, adc: f64) -> f64 {
